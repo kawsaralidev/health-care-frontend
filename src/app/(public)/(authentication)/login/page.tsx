693eb72f5c5a3402dff0773cf5373c0189511264
Md Kawsar Ali <mdkawsarali346@gmail.com>
2026-09-09 16:34:39 +0600

    create login form

--- src/app/(public)/(authentication)/login/page.tsx
+++ src/app/(public)/(authentication)/login/page.tsx
@@ -1,7 +1,21 @@
+import LoginForm from "@/components/form/loginForm";
+import Link from "next/link";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h2>this is login page</h2>
+    <div className="grid min-h-svh lg:grid-cols-1">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            PH Healthcare
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/layout/public/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-full h-16 border border-t flex justify-center items-center">
+      <h1> copyright: PH Healthcare </h1>
+    </div>
+  );
+};
+
+export default Footer;
